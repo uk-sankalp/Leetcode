981598b9d230a9dd924a389c4ce9775eaa9c8563
U K Sankalp <166250499+uk-sankalp@users.noreply.github.com>
2024-10-13 19:41:57 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (41.04%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public static int search(int[] arr,int target){
+        int s=0;
+        int e=arr.length-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            if(arr[s]<=arr[mid]){
+                if(arr[s]<=target && target<arr[mid]){
+                    e=mid-1;
+                }
+                else {
+                    s=mid+1;
+                }
+
+            }
+            else {
+                if(arr[mid]<target && target<=arr[e]){
+                    s=mid+1;
+                }
+                else{
+                    e=mid-1;
+                }
+            }
+
+        }
+        return -1;
+    }
+}
