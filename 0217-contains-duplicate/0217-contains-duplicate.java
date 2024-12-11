b9391728bf9a1c93e685915414e6e3ddd46abe62
U K Sankalp <166250499+uk-sankalp@users.noreply.github.com>
2024-12-12 00:08:40 +0530

    Time: 20 ms (23.45%), Space: 55.2 MB (88.40%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,8 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
+  public static boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
+        for (int i = 0; i < nums.length-1; i++) {
             if(nums[i]==nums[i+1]){
-                return true;
-            }
-        }
-        return false;
+                return true;}} return false;
     }
 }
