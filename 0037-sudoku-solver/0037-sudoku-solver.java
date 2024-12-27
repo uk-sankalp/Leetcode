506c7696e7ebcf15a5c3476fe405942b993c4238
U K Sankalp <166250499+uk-sankalp@users.noreply.github.com>
2024-12-27 17:03:08 +0530

    Update 0037-sudoku-solver.java

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -5,7 +5,7 @@ class Solution {
         for(int j = 0; j < 9; j++) {
             if(board[row][j]==num) return false;
         }   
-        // check col
+        // check in 
         for(int i = 0; i < 9; i++) {
             if(board[i][col]==num) return false;
         }        
