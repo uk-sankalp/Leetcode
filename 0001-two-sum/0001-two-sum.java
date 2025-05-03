ca8cbd60688de950399ce8ff6e7ecfdedad66238
Chill Panther <166250499+uk-sankalp@users.noreply.github.com>
2025-05-04 00:04:59 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,7 +4,7 @@ class Solution {
         for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             if (nums[i] + nums[j] == target) {
-                return new int[]{i, j};
+                return new int[]{i, j}; // the 2 indexes
                 }
             }
         }
