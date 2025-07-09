543db0a2cb694becccb4dfa83181b2ce0371ffb7
Chill Panther <166250499+uk-sankalp@users.noreply.github.com>
2025-07-09 19:02:30 +0530

    Time: 1 ms (100%), Space: 63.3 MB (40%) - LeetHub

---
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
+    int[] gap = new int[startTime.length +1];
+    gap[0] = startTime[0];
+    for(int i = 1; i < startTime.length; ++i) gap[i] = startTime[i] -  endTime[i-1];
+    gap[startTime.length] = eventTime - endTime[endTime.length-1];
+    int ans = 0, sum = 0;
+    for(int i = 0; i < gap.length; ++i){
+        sum += gap[i] - ((i >=  k+1)?gap[i - (k+1)]:0);
+        ans = Math.max(ans, sum);
+    }
+    return ans;
+}
+}
