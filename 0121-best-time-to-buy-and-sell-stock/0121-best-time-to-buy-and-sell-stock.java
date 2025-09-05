56966a19d56d0203e105bc1c2b6adf7f55711be1
Chill Panther <166250499+uk-sankalp@users.noreply.github.com>
2025-09-06 01:37:12 +0530

    Time: 1 ms (99.83%), Space: 61.5 MB (75.36%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,52 +1,30 @@
 class Solution {
-public int maxProfit(int[] prices) {
-    int buy=Integer.MAX_VALUE;
-    int max_profit=0;
+    public int maxProfit(int[] prices) {
+    //  -----------> Brute force Approach
+    //     int maxprofit=0;
+    //    for(int i=0;i<prices.length;i++){
+    //     int min=10000;
+    //     for(int j=i;j>=0;j--){
+    //         if(prices[j]<min){
+    //             min=prices[j];
+    //         }
+    //     }
+    //     int profit=prices[i]-min;
+    //     maxprofit=Math.max(maxprofit,profit);
+    //    } 
+    //    return maxprofit;
+
+    // ------> Optimised Approach
+    int maxprofit=0;
+    int buyingp=10000;
     for(int i=0;i<prices.length;i++){
-        if(buy<prices[i]){
-            int profit=prices[i]-buy;
-            max_profit=Math.max(max_profit,profit);
+        if(prices[i]<buyingp){
+            buyingp=prices[i];
         }
         else{
-            buy=prices[i];
+            maxprofit=Math.max(maxprofit,prices[i]-buyingp);
         }
     }
-    return max_profit;
-  }
+    return maxprofit;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-//     public int maxProfit(int[] prices) {
-//         int max=Integer.MIN_VALUE;
-//         int profit=0;
-//         for(int i=0;i<prices.length;i++){
-//             for(int j=i+1;j<prices.length;j++){
-//                 profit=prices[j]-prices[i];
-//                 max=Math.max(profit,max);
-//             }
-//         }
-//         if(max<0){
-//             max=0;
-//         }
-//         return max;
-//     }
-// }
