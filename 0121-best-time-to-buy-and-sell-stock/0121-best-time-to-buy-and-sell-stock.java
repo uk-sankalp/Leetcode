da85bd5bef582550c48f8cfe50ff3859b26a339d
Chill Panther <166250499+uk-sankalp@users.noreply.github.com>
2025-09-06 15:39:04 +0530

    Time: 1 ms (99.83%), Space: 61.3 MB (89.78%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,30 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-    //  -----------> Brute force Approach
-    //     int maxprofit=0;
-    //    for(int i=0;i<prices.length;i++){
-    //     int min=10000;
-    //     for(int j=i;j>=0;j--){
-    //         if(prices[j]<min){
-    //             min=prices[j];
-    //         }
-    //     }
-    //     int profit=prices[i]-min;
-    //     maxprofit=Math.max(maxprofit,profit);
-    //    } 
-    //    return maxprofit;
-
-    // ------> Optimised Approach
-    int maxprofit=0;
-    int buyingp=10000;
+    int profit=0;
+    int buy=prices[0];
     for(int i=0;i<prices.length;i++){
-        if(prices[i]<buyingp){
-            buyingp=prices[i];
+        if(prices[i]<buy){
+            buy=prices[i];
         }
         else{
-            maxprofit=Math.max(maxprofit,prices[i]-buyingp);
+            profit=Math.max(profit,prices[i]-buy);
         }
     }
-    return maxprofit;
+    return profit;
     }
 }
