157200530c2f0aa43ccd13cf77a401d50f224914
Chill Panther <166250499+uk-sankalp@users.noreply.github.com>
2025-09-13 23:50:42 +0530

    Time: 16 ms (14.19%), Space: 48.4 MB (5.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,17 @@
 class Solution {
-    public static boolean isPalindrome(String s) {
-    // StringBuilder sb=new StringBuilder();
-    String s1=(s.replaceAll("[^a-zA-Z0-9]", "")).toLowerCase();
-    int start=0;
-    int end=s1.length()-1;
-    while (start<end){
-        if (s1.charAt(start) == s1.charAt(end)) {
-            start++;
-            end--;
+   public static boolean isPalindrome(String s) {
+        String news=(s.replaceAll("[^a-zA-Z0-9]","")).toLowerCase();
+        int st=0;
+        int e=news.length()-1;
+        return palindrome(news,st,e);
+    }
+    public static boolean palindrome(String news,int st,int e){
+        if(st>=e){
+            return true;
         }
-        else {
+        if(news.charAt(st)!=news.charAt(e)){
             return false;
         }
-    }
-    return true;
+        return palindrome(news,st+1,e-1);
     }
 }
