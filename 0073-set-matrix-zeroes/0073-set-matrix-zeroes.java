79ca9841e07d61c2b708ba8e2259520ddecfcffd
Chill Panther <166250499+uk-sankalp@users.noreply.github.com>
2025-09-14 20:33:45 +0530

    Time: 0 ms (100%), Space: 46.2 MB (8.67%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,41 +1,90 @@
-class Pair{
-    int row;
-    int col;
-    Pair(int row,int col){
-        this.row=row;
-        this.col=col;
-    }
-}
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int[] ur={-1,0,1,0};
-        int[] uc={0,1,0,-1};
-        int[][] visited=new int[matrix.length][matrix[0].length];
-        Queue<Pair> q=new LinkedList<>();
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==0){
-                    q.add(new Pair(i,j));
+    // -------------Brute Force------------------
+
+    //     int n=matrix.length;
+    //     int m=matrix[0].length;
+    //    boolean[][] visited=new boolean[n][m];
+    //    for(int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++){
+    //         if(matrix[i][j]==0){
+    //             markvisited(visited,i,j);
+    //         }
+    //     }
+    //    }
+    //    for(int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++){
+    //         if(visited[i][j]==true){
+    //             matrix[i][j]=0;
+    //         }
+    //     }
+    //    }
+    // }
+    // public static void markvisited(boolean[][] visited,int i,int j){
+    //     for(int s=0;s<visited[0].length;s++){
+    //         if(!visited[i][s]){
+    //             visited[i][s]=true;
+    //         }
+    //     }
+    //     for(int s=0;s<visited.length;s++){
+    //         if(!visited[s][j]){
+    //             visited[s][j]=true;
+    //         }
+    //     }
+
+    // ------------------------Better Approach---------
+    // int n=matrix.length;
+    // int m=matrix[0].length;
+    // int[] row=new int[n];
+    // int[] col=new int[m];
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++){
+    //         if(matrix[i][j]==0){
+    //             row[i]=col[j]=1;
+    //         }
+    //     }
+    // }
+    //   for(int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++){
+    //        if(row[i]==1 || col[j]==1){
+    //         matrix[i][j]=0;
+    //        }
+    //     }
+    // }
+
+    // ------------------------------Optimal Approach----------------------------------
+    int col0=1;
+    int n=matrix.length;
+    int m=matrix[0].length;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(matrix[i][j]==0){
+                matrix[i][0]=0;
+                if(j!=0){
+                    matrix[0][j]=0;
+                }
+                else{
+                    col0=0;
                 }
             }
         }
-        while(!q.isEmpty()){
-            int r=q.peek().row;
-            int c=q.peek().col;
-            q.remove();
-            for(int i=0;i<matrix.length;i++){
-                matrix[i][c]=0;
-            }
-            for(int i=0;i<matrix[0].length;i++){
-                matrix[r][i]=0;
-            }
-            // for(int i=0;i<4;i++){
-            //     int nr=r+ur[i];
-            //     int nc=c+uc[i];
-            //     if(nr>=0 && nc>=0 && nr<matrix.length && nc<matrix[0].length){
-            //         matrix[nr][nc]=0;
-            //     }
-            // }
+    }
+    for(int i=1;i<n;i++){
+        for(int j=1;j<m;j++){
+              if(matrix[i][0]==0 || matrix[0][j]==0){
+                matrix[i][j]=0;
+              }
         }
+    }
+    if(matrix[0][0]==0){
+        for(int i=0;i<m;i++){
+            matrix[0][i]=0;
+        }
+    }
+    if(col0==0){
+        for(int i=0;i<n;i++){
+            matrix[i][0]=0;
+        }
+    }
     }
 }
