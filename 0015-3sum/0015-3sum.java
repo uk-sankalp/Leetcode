1a2f19aa61a16347e3d5b12bdb89239d21ac8dfe
Chill Panther <166250499+uk-sankalp@users.noreply.github.com>
2025-09-22 22:37:31 +0530

    Time: 30 ms (70.84%), Space: 52.9 MB (13.58%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -23,26 +23,62 @@ class Solution {
         // return ans;
 
         //Better Solution ~ O(N^2) * O(log n) for hashseet
-        List<List<Integer>> ans=new ArrayList<>();
-        Set<List<Integer>> set=new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            HashMap<Integer,Integer> hmap=new HashMap<>();
-            for(int j=i+1;j<nums.length;j++){
-                int val=-(nums[i]+nums[j]);
-                if(hmap.containsKey(val)){
-                    List<Integer> temp=new ArrayList<>();
-                    temp.add(nums[i]);
-                    temp.add(nums[j]);
-                    temp.add(val);
-                    Collections.sort(temp);
-                    set.add(temp);
-                }
-                hmap.put(nums[j],0);
+        // List<List<Integer>> ans=new ArrayList<>();
+        // Set<List<Integer>> set=new HashSet<>();
+        // for(int i=0;i<nums.length;i++){
+        //     HashMap<Integer,Integer> hmap=new HashMap<>();
+        //     for(int j=i+1;j<nums.length;j++){
+        //         int val=-(nums[i]+nums[j]);
+        //         if(hmap.containsKey(val)){
+        //             List<Integer> temp=new ArrayList<>();
+        //             temp.add(nums[i]);
+        //             temp.add(nums[j]);
+        //             temp.add(val);
+        //             Collections.sort(temp);
+        //             set.add(temp);
+        //         }
+        //         hmap.put(nums[j],0);
+        //     }
+        // }
+        // for(List<Integer> item:set){
+        //     ans.add(item);
+        // }
+        // return ans;
+
+    // Optimal Approach
+    Arrays.sort(nums);
+    List<List<Integer>> ans=new ArrayList<>();
+    for(int i=0;i<nums.length;i++){
+        if(i!=0 && nums[i]==nums[i-1]){
+            continue;
+        }
+        int j=i+1;
+        int k=nums.length-1;
+        while(j<k){
+            int sum=nums[i]+nums[j]+nums[k];
+            if(sum==0){
+            List<Integer> temp=new ArrayList<>();
+            temp.add(nums[i]);
+            temp.add(nums[j]);
+            temp.add(nums[k]);
+            ans.add(temp);
+            j++;
+            k--;
+            while(j<k && nums[j]==nums[j-1]){
+                j++;
+            }
+            while(j<k && nums[k]==nums[k+1]){
+                k--;
             }
         }
-        for(List<Integer> item:set){
-            ans.add(item);
+        else if(sum<0){
+            j++;
         }
-        return ans;
+        else {
+            k--;
+         }
+        }
+    }
+    return ans;
     }
 }
