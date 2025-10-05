bc52d036f303177e20b29aab88277c19ee5b1bbb
Chill Panther <166250499+uk-sankalp@users.noreply.github.com>
2025-10-05 12:12:11 +0530

    Time: 0 ms (100%), Space: 42.9 MB (83.67%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static int lowerbound(int[] nums,int target){
+        int i=0;
+        int j=nums.length-1;
+        int ans=nums.length;
+        while(i<=j){
+            int mid=i+((j-i)/2);
+            if(nums[mid]>=target){
+                ans=mid;
+                j=mid-1;
+            }
+            else{
+            i=mid+1;
+            }
+        }
+        return ans;
+    }
+    public int searchInsert(int[] nums, int target) {
+        int ans=lowerbound(nums,target);
+        return ans;
+    }
+}
