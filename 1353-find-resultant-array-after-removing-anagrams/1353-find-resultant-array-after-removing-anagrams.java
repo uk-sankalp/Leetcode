69d213d1186c1a6e606c9a20b6f68d591746c81a
Chill Panther <166250499+uk-sankalp@users.noreply.github.com>
2025-10-13 23:49:19 +0530

    Time: 3 ms (55.46%), Space: 44.8 MB (65.92%) - LeetHub

---
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static boolean isSame(char[] arr, char [] arr2, int n){
+        for(int i=0;i<n;i++){
+            if(arr[i]!=arr2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public List<String> removeAnagrams(String[] words) {
+        List<String> ans=new ArrayList<>();
+        List<String> dummy=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            dummy.add(words[i]);
+        }
+        for(int i=words.length-1;i>0;i--){
+            if(words[i].length()==words[i-1].length()){
+                char[] charArray1 = words[i].toCharArray();
+                char[] charArray2 = words[i-1].toCharArray();
+                Arrays.sort(charArray1);
+                Arrays.sort(charArray2);
+                if(isSame(charArray1,charArray2,charArray1.length)){
+                    dummy.remove(i);
+                }
+            }
+        }
+        return dummy;
+    }
+}
