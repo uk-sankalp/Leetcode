aa3167d9c34b66b16f7eb3d6aea691257dbdf4ab
Chill Panther <166250499+uk-sankalp@users.noreply.github.com>
2025-11-08 01:00:07 +0530

    Time: 2 ms (99.11%), Space: 46.8 MB (5.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,32 +1,13 @@
-import java.util.HashMap;
-
-public class TwoSum {
-    public static int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>(); // value -> index
-        
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            
-            // check if complement already exists
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[] {map.get(target-nums[i]),i};
             }
-            
-            // store current number with its index
-            map.put(nums[i], i);
+            map.put(nums[i],i);
         }
-        
-        // if no pair found (though problem says there is always one)
-        throw new IllegalArgumentException("No two sum solution");
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        
-        int[] result = twoSum(nums, target);
-        System.out.println("Indices: " + result[0] + ", " + result[1]);
+        return new int[] {};
     }
 }
-
-        
